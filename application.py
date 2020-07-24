9b77f36c3f554c6d804756415772f17fa0da1d1f
KapoorAyush <akapoor.gm2000@gmail.com>
2020-07-24 16:14:16 +0530

    extracts

--- application.py
+++ application.py
@@ -22,6 +22,89 @@ y_tr=np.delete(y_tr,-1)
 max_text_len=40
 max_summary_len=10
 
+#!/usr/bin/env python
+# coding: utf-8
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import networkx as nx
+def read_article(file):
+#     file = open(file_name, "r")
+    filedata = file.split('\n')
+    article = filedata[0].split(". ")
+    sentences = []
+
+    for sentence in article:
+#         print(sentence)
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop() 
+    
+    return sentences
+
+def sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+ 
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+ 
+    all_words = list(set(sent1 + sent2))
+ 
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+ 
+    # build the vector for the first sentence
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+ 
+    # build the vector for the second sentence
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+ 
+    return 1 - cosine_distance(vector1, vector2)
+ 
+def build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+ 
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: #ignore if both are same sentences
+                continue 
+            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+    return similarity_matrix
+
+
+def generate_summary(file_name):
+    stop_words = stopwords.words('english')
+    summarize_text = []
+
+    # Step 1 - Read text anc split it
+    sentences =  read_article(file_name)
+    top_n=int(len(sentences)/2)
+    # Step 2 - Generate Similary Martix across sentences
+    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+#     print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+
+    for i in range(top_n):
+        summarize_text.append(" ".join(ranked_sentence[i][1]))
+#         print(" ".join(ranked_sentence[i][1]))
+    return summarize_text
+    # Step 5 - Offcourse, output the summarize texr
+#     print("Summarize Text: \n", ". ".join(summarize_text))
+
+
 x_tokenizer = Tokenizer() 
 x_tokenizer.fit_on_texts(list(x_tr))
 
@@ -182,12 +265,16 @@ def calc_mpg():
     errors = []
 
     x = content['text']
+    points=generate_summary(x)
+    npts=len(points)
     txt=['sostok '+text_cleaner(x,0)+' eostok']
     txt=pad_sequences( x_tokenizer.texts_to_sequences(txt), maxlen=max_text_len, padding='post')
     pred=decode_sequence(txt.reshape(1,max_text_len))
     summary = str(pred)
-    response = {"id":str(uuid.uuid4()),"summary":summary}
-    print(content['text'])
+    response = {"id":str(uuid.uuid4()),"summary":summary,"npts":npts}
+    for i in range(1,npts+1):
+        response.update({"point"+str(i):points[i-1]})
+    # print(content['text'])
     return jsonify(response)
 
 if __name__ == '__main__':
